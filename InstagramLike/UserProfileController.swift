8b51911176e3816c09c186bcdfb4e39a09f2556c
Gonzalo Reyes <gonzalorehu.97.31@gmail.com>
2017-06-07 20:44:26 -0500

    Profile Header and Grid Cells

--- InstagramLike/UserProfileController.swift
+++ InstagramLike/UserProfileController.swift
@@ -14,6 +14,7 @@ class UserProfileController: UICollectionViewController {
     // MARK: - Object Ids
     
     private let headerId = "headerId"
+    private let cellId = "cellId"
     
     
     // MARK - View Lifecycle
@@ -26,9 +27,9 @@ class UserProfileController: UICollectionViewController {
     }
     var user: User?
     fileprivate func fetchUser() {
-        guard let uid = FIRAuth.auth()?.currentUser?.uid else { print("FirstAylmao");return }
+        guard let uid = FIRAuth.auth()?.currentUser?.uid else { return }
         FIRDatabase.database().reference().child("users").child(uid).observeSingleEvent(of: .value, with: { (snapshot) in
-            guard let dictionary = snapshot.value as? [String: Any] else { print("Ay lmao");return }
+            guard let dictionary = snapshot.value as? [String: Any] else { return }
             self.user = User(dictionary: dictionary)
             self.navigationItem.title = self.user?.username
             self.collectionView?.reloadData()
@@ -39,6 +40,7 @@ class UserProfileController: UICollectionViewController {
     
     fileprivate func registerViews() {
         collectionView?.register(UserProfileHeader.self, forSupplementaryViewOfKind: UICollectionElementKindSectionHeader, withReuseIdentifier: headerId)
+        collectionView?.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellId)
     }
     
     
@@ -49,6 +51,16 @@ class UserProfileController: UICollectionViewController {
         header.user = self.user
         return header
     }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 7
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        cell.backgroundColor = .purple
+        return cell
+    }
 }
 
 
@@ -57,5 +69,15 @@ class UserProfileController: UICollectionViewController {
 extension UserProfileController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
         return CGSize(width: view.frame.width, height: 200)
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = (view.frame.width-2)/3
+        return CGSize(width: width, height: width)
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 1
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 1
     }
 }
--- InstagramLike/UserProfileHeader.swift
+++ InstagramLike/UserProfileHeader.swift
@@ -15,35 +15,110 @@ class UserProfileHeader: UICollectionViewCell {
     
     var user: User? {
         didSet {
-            print("Hello")
             setupProfileImage()
+            usernameLabel.text = user?.username
         }
     }
-    
     
     // MARK: - Interface Objects
     
     let profileImageView: UIImageView = {
         let iv = UIImageView()
-        iv.backgroundColor = .red
         iv.layer.cornerRadius = 80/2
         iv.clipsToBounds = true
         return iv
     }()
     
+    let gridButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName: "grid"), for: .normal)
+//        button.tintColor = UIColor(white: 0, alpha: 0.2)
+        return button
+    }()
+    
+    let listButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName: "list"), for: .normal)
+        button.tintColor = UIColor(white: 0, alpha: 0.2)
+        return button
+    }()
+    
+    let bookmarkButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName: "ribbon"), for: .normal)
+        button.tintColor = UIColor(white: 0, alpha: 0.2)
+        return button
+    }()
+    
+    let usernameLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 14)
+        return label
+    }()
+    
+    let postsLabel: UILabel = {
+        let label = UILabel()
+        
+        let attributedText = NSMutableAttributedString(string: "0\n", attributes: [NSFontAttributeName:UIFont.boldSystemFont(ofSize: 14)])
+        
+        attributedText.append(NSAttributedString(string: "posts", attributes: [NSForegroundColorAttributeName:UIColor.lightGray, NSFontAttributeName:UIFont.systemFont(ofSize: 14)]))
+        
+        label.attributedText = attributedText
+        label.numberOfLines  = 0
+        label.textAlignment = .center
+        return label
+    }()
+    
+    let followersLabel: UILabel = {
+        let label = UILabel()
+        let attributedText = NSMutableAttributedString(string: "0\n", attributes: [NSFontAttributeName:UIFont.boldSystemFont(ofSize: 14)])
+        
+        attributedText.append(NSAttributedString(string: "followers", attributes: [NSForegroundColorAttributeName:UIColor.lightGray, NSFontAttributeName:UIFont.systemFont(ofSize: 14)]))
+        
+        label.attributedText = attributedText
+        label.numberOfLines  = 0
+        label.textAlignment = .center
+        return label
+    }()
+    
+    let followingLabel: UILabel = {
+        let label = UILabel()
+        let attributedText = NSMutableAttributedString(string: "0\n", attributes: [NSFontAttributeName:UIFont.boldSystemFont(ofSize: 14)])
+        
+        attributedText.append(NSAttributedString(string: "following", attributes: [NSForegroundColorAttributeName:UIColor.lightGray, NSFontAttributeName:UIFont.systemFont(ofSize: 14)]))
+        
+        label.attributedText = attributedText
+        label.numberOfLines  = 0
+        label.textAlignment = .center
+        return label
+    }()
+    
+    let editProfileButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Edit Profile", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.titleLabel?.font = .boldSystemFont(ofSize: 14)
+        button.layer.borderColor = UIColor.lightGray.cgColor
+        button.layer.borderWidth = 1
+        button.layer.cornerRadius = 3
+        return button
+    }()
     
     // MARK: - View Lifecycle
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        backgroundColor = .blue
-        setupViews()
+        setupProfileImageView()
+        setupBottomToolBar()
+        setupUsernameLabel()
+        setupUserStatsViews()
+        setupEditProfileButton()
     }
     
     
     // MARK: - Setup Methods
     
-    fileprivate func setupViews() {
+    fileprivate func setupProfileImageView() {
         addSubview(profileImageView)
         profileImageView.snp.makeConstraints { (make) in
             make.top.left.equalTo(self).offset(12)
@@ -52,15 +127,14 @@ class UserProfileHeader: UICollectionViewCell {
     }
     
     fileprivate func setupProfileImage() {
-        print("yo waddup")
-        guard let profileImageUrl = user?.profileImageUrl else { print("1");return }
-        guard let url = URL(string: profileImageUrl) else { print("2");return }
+        guard let profileImageUrl = user?.profileImageUrl else { return }
+        guard let url = URL(string: profileImageUrl) else { return }
         URLSession.shared.dataTask(with: url) { (data, response, error) in
             if let err = error {
                 print("Failed to fetch profile image: ", err)
                 return
             }
-            guard let data = data else { print("3");return }
+            guard let data = data else { return }
             let image = UIImage(data: data)
             
             DispatchQueue.main.async {
@@ -69,6 +143,67 @@ class UserProfileHeader: UICollectionViewCell {
             }.resume()
     }
     
+    fileprivate func setupUsernameLabel() {
+        addSubview(usernameLabel)
+        usernameLabel.snp.makeConstraints { (make) in
+            make.left.equalTo(self).offset(12)
+            make.right.equalTo(self).offset(-12)
+            make.top.equalTo(profileImageView.snp.bottom).offset(4)
+            make.bottom.equalTo(gridButton.snp.top)
+        }
+    }
+    
+    fileprivate func setupBottomToolBar() {
+        let topDividerView = UIView()
+        topDividerView.backgroundColor = .lightGray
+        
+        let bottomDividerView = UIView()
+        bottomDividerView.backgroundColor = .lightGray
+        
+        let stackView = UIStackView(arrangedSubviews: [gridButton, listButton,  bookmarkButton])
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        
+        addSubview(stackView)
+        addSubview(topDividerView)
+        addSubview(bottomDividerView)
+        stackView.snp.makeConstraints { (make) in
+            make.left.right.bottom.equalTo(self)
+            make.height.equalTo(50)
+        }
+        topDividerView.snp.makeConstraints { (make) in
+            make.left.right.equalTo(self)
+            make.bottom.equalTo(stackView.snp.top)
+            make.height.equalTo(0.5)
+        }
+        bottomDividerView.snp.makeConstraints { (make) in
+            make.left.right.equalTo(self)
+            make.top.equalTo(stackView.snp.bottom)
+            make.height.equalTo(0.5)
+        }
+    }
+    
+    fileprivate func setupUserStatsViews() {
+        let stackview = UIStackView(arrangedSubviews: [postsLabel, followersLabel, followingLabel])
+        stackview.distribution = .fillEqually
+        addSubview(stackview)
+        stackview.snp.makeConstraints { (make) in
+            make.top.equalTo(self).offset(12)
+            make.left.equalTo(profileImageView.snp.right).offset(12)
+            make.right.equalTo(self).offset(-12)
+            make.height.equalTo(50)
+        }
+    }
+    
+    fileprivate func setupEditProfileButton() {
+        addSubview(editProfileButton)
+        editProfileButton.snp.makeConstraints { (make) in
+            make.top.equalTo(postsLabel.snp.bottom).offset(2)
+            make.left.equalTo(postsLabel)
+            make.right.equalTo(followingLabel)
+            make.height.equalTo(34)
+        }
+    }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
